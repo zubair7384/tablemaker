60573de5392752347568db87faa9f787ae9fad67
zubair7384 <zubairiqbal@uconnect.pk>
2018-12-18 17:05:54 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var rows = +prompt("How many rows for your multiplication table?");
+var cols = +prompt("How many columns for your multiplication table?");
+if (rows == "" || rows == null) alert("Please input number of rows");
+if (cols == "" || cols == null) alert("Please input number of cols");
+createTable(rows, cols);
+function createTable(rows, cols) {
+  var j = 1;
+  var output = "<table border='1' width='500' cellspacing='0'cellpadding='5'>";
+  for (i = 1; i <= rows; i++) {
+    output = output + "<tr>";
+    while (j <= cols) {
+      output = output + "<td>" + i * j + "</td>";
+      j = j + 1;
+    }
+    output = output + "</tr>";
+    j = 1;
+  }
+  output = output + "</table>";
+  document.write(output);
+}
